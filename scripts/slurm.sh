065d24c6a55ccd95d5f5b9e94896fc1915d189af
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-05-22 13:07:24 -0400

    Adding slurm script

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -6,15 +6,15 @@
 #SBATCH --mail-type=ALL
 
 # Environment variables
-CONTAINER_PATH="/explore/nobackup/projects/ilab/projects/LobodaTFO/software/containers/wrf-baselibs"
-CONFIG_PATH="/explore/nobackup/people/jacaraba/development/wildfire-occurrence/projects/wrf/config/experiments-reproducibility.yaml"
+CONTAINER_PATH="REPLACE_WITH_CONTAINER_PATH"
+CONFIG_PATH="/explore/nobackup/people/jacaraba/development/above-shrubs/projects/chm_cnn/configs/above_shrubs_cnn_v1.yaml"
 
 # 1. Load singularity module
 module load singularity
 
-# 2. Download singularity container if not available
-#if [ ! -d /explore/nobackup/projects/ilab/projects/LobodaTFO/software/containers/wrf-baselibs ]; then
-#  singularity build --sandbox /lscratch/jacaraba/container/wrf-baselibs docker://kkeene44/wrf-coop:version16
-#fi
-
-srun -n 1 singularity exec --env PYTHONPATH="$NOBACKUP/development/above-shrubs" --nv -B $NOBACKUP,/lscratch,/explore/nobackup/people /lscratch/$USER/container/above-shrubs python $NOBACKUP/development/above-shrubs/above_shrubs/view/chm_pipeline_cnn.py -c /explore/nobackup/people/jacaraba/development/above-shrubs/projects/chm_cnn/configs/above_shrubs_cnn_v1.yaml -s preprocess
+# 2. Execute software, one of setup, preprocess, train, predict, validate
+srun -n 1 singularity exec --env PYTHONPATH="$NOBACKUP/development/above-shrubs" --nv \
+     -B $NOBACKUP,/lscratch,/explore/nobackup/people /lscratch/$USER/container/above-shrubs \
+     python $NOBACKUP/development/above-shrubs/above_shrubs/view/chm_pipeline_cnn.py \
+     -c /explore/nobackup/people/jacaraba/development/above-shrubs/projects/chm_cnn/configs/above_shrubs_cnn_v1.yaml \
+     -s preprocess
